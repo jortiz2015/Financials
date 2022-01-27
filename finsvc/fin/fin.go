2aab6ef23ecf2f4d7a20b33ecd8b653c726d21d2
Jose Ortiz <jose.antonio.ortiz.jr@gmail.com>
2022-01-27 12:49:24 -0700

    Used as a simple test to test the FinSvcServer interface

--- finsvc/fin/fin.go
+++ finsvc/fin/fin.go
@@ -2,7 +2,7 @@ package fin
 
 import (
 	"context"
-	pb "finsvc/finsvc/pb"
+	pb "finsvc/pb"
 	"log"
 	"time"
 
@@ -20,14 +20,14 @@ func NewFin(log *log.Logger) *Fin {
 func (f *Fin) GetBalanceSheet(ctx context.Context, ticker *pb.Ticker) (*pb.BalanceSheet, error) {
 	f.log.Println("GetBalanceSheet called: ", "ticker", ticker.GetSymbol())
 
-	return &pb.BalanceSheet{ReportDate: timestamppb.New(time.Now()), FillingType: "10k",
+	return &pb.BalanceSheet{ReportDate: timestamppb.New(time.Now()), FilingType: "10k",
 		FiscalDate: timestamppb.New(time.Now()), FiscalQuarter: 1, FiscalYear: 2022,
 		Currency: "USD", CurrentCash: 420.69, ShortTermInvestments: 420.69, Receivables: 420.69,
 		Inventory: 420.69, OtherCurrentAssets: 420.69, CurrentAssets: 420.69,
 		LongTermInvestments: 420.69, PropertyPlantEquipment: 420.69, Goodwill: 420.69,
 		IntangibleAssets: 420.69, OtherAssets: 420.69, TotalAssets: 420.69, AccountsPayable: 420.69,
 		CurrentLongTermDebt: 420.69, OtherCurrentLiabilities: 420.69, TotalCurrentLiabilities: 420.69,
-		LongTermDebt: 420.69, OtherLiablities: 420.69, MinorityInterest: 420.69,
+		LongTermDebt: 420.69, OtherLiabilities: 420.69, MinorityInterest: 420.69,
 		TotalLiabilities: 420.69, CommonStock: 420.69, RetainedEarnings: 420.69,
 		TreasuryStock: 420.69, CapitalSurplus: 420.69, ShareholderEquity: 420.69,
 		NetTangibleAssets: 420.69}, nil
