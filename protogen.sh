2512ae57235e176472b4779defc67976d64825eb
Jose Ortiz <jose.antonio.ortiz.jr@gmail.com>
2022-02-04 07:39:05 -0700

    Added grpc-web proto

--- protogen.sh
+++ protogen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 protoc -I=./pb finsvc.proto \
-    --go_out=plugins=grpc:./finsvc/pb #\
-#    --js_out=import_style=commonjs,binary:./path/to/desired/typescript/output \
-#    --grpc-web_out=import_style=typescript,mode=grpcweb:./path/to/desired/typescript/output
+    --go_out=plugins=grpc:./finsvc/pb \
+    --js_out=import_style=commonjs,binary:./web/pages \
+    --grpc-web_out=import_style=typescript,mode=grpcweb:./web/pages
 
